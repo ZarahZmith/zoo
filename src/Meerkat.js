feeb8add83f6a69641e4167159c68f67035da3f8
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-20 21:07:57 -0400

    Added doc block to the Meerkat's functions

--- src/Meerkat.js
+++ src/Meerkat.js
@@ -11,22 +11,37 @@
    */
   window.zoo.Meerkat = class Meerkat extends Animal {
 
+    /**
+     * Re-establishes the arguments in the parent function in Animal.js
+     */
     constructor(name, dateOfBirth) {
       super(name, dateOfBirth);
     }
 
+    /**
+     * Creates a function to constantly return the scientific name of the animal that is not subject to change
+     */
     static scientificName() {
       return 'Suricata suricatta';
     }
 
+    /**
+     * Creates the function for the specific animal's birth outcome
+     */
     giveBirth(meerkat) {
       this.giveBirth = giveBirth;
     }
 
+    /**
+     * This function calculates the speed and depth the animal burrowed to return the burrow efficiency
+     */
     burrowEfficiency(speed, depth) {
       return (speed * depth);
     }
 
+    /**
+     * Returns the name of the anaimal as well as a string which related it to it's scientific name
+     */
     toString() {
       return this.name + ', scienficically known as ' + this.scientificName;
     }
