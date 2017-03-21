a631f4b89db1c95c7976f449e47567d916601df0
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-20 21:56:57 -0400

    Fixed spelling error

--- src/Elephant.js
+++ src/Elephant.js
@@ -12,8 +12,8 @@
   window.zoo.Elephant = class Elephant extends Animal {
 
     /**
-    * Re-establishes the arguments of the parent function in Animal.js
-    */
+     * Re-establishes the arguments of the parent function in Animal.js
+     */
     constructor(name, dateOfBirth) {
       super(name, dateOfBirth);
     }
@@ -23,6 +23,28 @@
      */
     static scientificName() {
       return 'Loxodonta';
+    }
+
+    /**
+     * Creates the function for the specific animal's birth outcome
+     */
+    giveBirth(elephant) {
+      this.giveBirth = giveBirth;
+    }
+
+    /**
+     * Method by which the Animal migrates calculated by distance times time
+     */
+    migrate(distance, time) {
+      return (distance * time);
+    }
+
+
+    /**
+     * Returns the name of the anaimal as well as a string which related it to it's scientific name
+     */
+    toString() {
+      return this.name + ', scienficically known as ' + this.scientificName;
     }
 
   };
