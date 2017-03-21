4b6154e0b061f3749f0ec897330f524c24fe991e
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-20 21:56:13 -0400

    Logged out the basic information of a Meerkat named Meera

---
+++ src/zoo.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  let Animal = window.zoo.Animal;
+  let Meerkat = window.zoo.Meerkat;
+  let Elephant = window.zoo.Elephant;
+
+  let meera = new Meerkat('Meera', new Date('3/5/2012'));
+  console.log(meera);
+
+}());
