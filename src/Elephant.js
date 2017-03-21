e74f72ac741f12307c76ce39e6077c0ba9a25cbf
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-20 21:33:06 -0400

    Established static scietific name as well as parent elements

--- src/Elephant.js
+++ src/Elephant.js
@@ -1,4 +1,31 @@
 (function() {
   'use strict';
-  
+
+  window.zoo = window.zoo || {};
+
+  let Animal = window.zoo.Animal;
+
+  /**
+   * Connects the individual animal to it's parent class, in this case Animal
+   * @type {Object}
+   */
+  window.zoo.Elephant = class Elephant extends Animal {
+
+    /**
+    * Re-establishes the arguments of the parent function in Animal.js
+    */
+    constructor(name, dateOfBirth) {
+      super(name, dateOfBirth);
+    }
+
+    /**
+     * Creates a function to constantly return the scientific name of the animal, the scientific name is not subject to change
+     */
+    static scientificName() {
+      return 'Loxodonta';
+    }
+
+  };
+
+
 }());
