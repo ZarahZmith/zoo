2e998cd75de1c141aef2babc48488f044d1cc304
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-20 21:00:58 -0400

    Gave Meerkat birth method, burrow method, and to return a string

--- src/Meerkat.js
+++ src/Meerkat.js
@@ -3,6 +3,8 @@
 
   window.zoo = window.zoo || {};
 
+  let Animal = window.zoo.Animal;
+
   /**
    * Function that establishes the features of an individual animal, in this case a Meerkat, using Animal.js as the parent function
    * @type {Object}
@@ -10,13 +12,23 @@
   window.zoo.Meerkat = class Meerkat extends Animal {
 
     constructor(name, dateOfBirth) {
-
       super(name, dateOfBirth);
-
     }
 
     static scientificName() {
       return 'Suricata suricatta';
+    }
+
+    giveBirth(meerkat) {
+      this.giveBirth = giveBirth;
+    }
+
+    burrowEfficiency(speed, depth) {
+      return (speed * depth);
+    }
+
+    toString() {
+      return this.name + ', scienficically known as ' + this.scientificName;
     }
 
   };
